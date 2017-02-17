e4092c7ea90a3f3fe6e194eb35c707558601470e
Megan Warnock <megandwarnock@gmail.com>
2017-02-17 09:44:25 -0800

    Add JS branching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
   $("#trackerForm").submit(function(event){
     var firstName = $("input#firstName").val();
+    var codingTrack = $("input:radio[name=codingTrack]:checked").val();
 
     $(".firstName").text(firstName);
 
+    if (codingTrack === "1"){
+      $("#javaAndroidTrack").show();
+    } else if (codingTrack === "2") {
+      $("#cssDesignTrack").show();
+    } else if (codingTrack === "3") {
+      $("#phpDrupalTrack").show();
+    }
 
-    $("#cssDesignTrack").show();
+    console.log(codingTrack)
     event.preventDefault();
   });
 });
