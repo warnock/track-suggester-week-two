800420e7c903b87fa3e9d65f171dcbc690a7f1bf
Megan Warnock <megandwarnock@gmail.com>
2017-02-17 10:32:57 -0800

    Add content to #javaAndroidTrack, #cssDesignTrack, and #phpDrupalTrack divs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
   $("#trackerForm").submit(function(event){
     var firstName = $("input#firstName").val();
+    var firstName = firstName.toUpperCase();
     var codingTrack = $("input:radio[name=codingTrack]:checked").val();
+    var city = $("input#city").val();
 
     $(".firstName").text(firstName);
+    $(".city").text(city);
 
     if (codingTrack === "1"){
       $("#javaAndroidTrack").show();
+      $("#cssDesignTrack").hide();
+      $("#phpDrupalTrack").hide();
     } else if (codingTrack === "2") {
       $("#cssDesignTrack").show();
+      $("#javaAndroidTrack").hide();
+      $("#phpDrupalTrack").hide();
     } else if (codingTrack === "3") {
       $("#phpDrupalTrack").show();
+      $("#cssDesignTrack").hide();
+      $("#javaAndroidTrack").hide();
     }
 
     console.log(codingTrack)
