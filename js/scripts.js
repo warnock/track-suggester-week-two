f83f5242664c0bd162415cbd23c959d92244c02e
Megan Warnock <megandwarnock@gmail.com>
2017-02-20 20:36:52 -0800

    Add new redio buttons and survey questions.

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,47 @@ $(document).ready(function(){
     var codingTrack = $("input:radio[name=codingTrack]:checked").val();
     var city = $("input#city").val();
 
-    $(".firstName").text(firstName);
-    $(".city").text(city);
-
-    if (codingTrack === "1", "2","3") {
-      $("#pleaseSelect").show();
-      $("#cssDesignTrack").hide();
-      $("#phpDrupalTrack").hide();
-      $("#javaAndroidTrack").hide();
-    }
-
-    if (codingTrack === "1"){
-      $("#javaAndroidTrack").show();
-      $("#cssDesignTrack").hide();
-      $("#phpDrupalTrack").hide();
-      $("#pleaseSelect").hide();
-    } else if (codingTrack === "2") {
-      $("#cssDesignTrack").show();
-      $("#javaAndroidTrack").hide();
-      $("#phpDrupalTrack").hide();
-      $("#pleaseSelect").hide();
-    } else if (codingTrack === "3") {
-      $("#phpDrupalTrack").show();
-      $("#cssDesignTrack").hide();
-      $("#javaAndroidTrack").hide();
-      $("#pleaseSelect").hide();
-    }
 
     event.preventDefault();
   });
 
-  $(".btn").click(function() {
-    $("#phpDrupalTrack").fadeIn();
-    $("#cssDesignTrack").fadeIn();
-    $("#javaAndroidTrack").fadeIn();
-    $("#pleaseSelect").fadeIn();
-  });
-  
+
 });
+
+
+
+//
+// $(".firstName").text(firstName);
+// $(".city").text(city);
+//
+// if (codingTrack === "1", "2","3") {
+//   $("#pleaseSelect").show();
+//   $("#cssDesignTrack").hide();
+//   $("#phpDrupalTrack").hide();
+//   $("#javaAndroidTrack").hide();
+// }
+//
+// if (codingTrack === "1"){
+//   $("#javaAndroidTrack").show();
+//   $("#cssDesignTrack").hide();
+//   $("#phpDrupalTrack").hide();
+//   $("#pleaseSelect").hide();
+// } else if (codingTrack === "2") {
+//   $("#cssDesignTrack").show();
+//   $("#javaAndroidTrack").hide();
+//   $("#phpDrupalTrack").hide();
+//   $("#pleaseSelect").hide();
+// } else if (codingTrack === "3") {
+//   $("#phpDrupalTrack").show();
+//   $("#cssDesignTrack").hide();
+//   $("#javaAndroidTrack").hide();
+//   $("#pleaseSelect").hide();
+// }
+//
+//
+//   $(".btn").click(function() {
+//     $("#phpDrupalTrack").fadeIn();
+//     $("#cssDesignTrack").fadeIn();
+//     $("#javaAndroidTrack").fadeIn();
+//     $("#pleaseSelect").fadeIn();
+//   });
