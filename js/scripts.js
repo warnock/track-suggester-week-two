fcfd455b790addafdc2387f2d55e9ede12097354
Megan Warnock <megandwarnock@gmail.com>
2017-02-22 20:29:45 -0800

    Add branching for 5 radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,54 @@
+
+
+
+
+
+
+
+//interface logic
 $(document).ready(function(){
   $("#trackerForm").submit(function(event){
     var firstName = $("input#firstName").val();
     var firstName = firstName.toUpperCase();
-    var codingTrack = $("input:radio[name=codingTrack]:checked").val();
+    var questionOne = parseInt($("input:radio[name=questionOne]:checked").val());
+    var questionTwo = parseInt($("input:radio[name=questionTwo]:checked").val());
+    var questionThree = parseInt($("input:radio[name=questionThree]:checked").val());
+    var questionFour = parseInt($("input:radio[name=questionFour]:checked").val());
+    var questionFive = parseInt($("input:radio[name=questionFive]:checked").val());
     var city = $("input#city").val();
-
-
     event.preventDefault();
+
+    var totalScore = questionOne + questionTwo + questionThree + questionFour + questionFive;
+
+
+
+    if (totalScore <= 5){
+      $("#cssDesignTrack").show();
+      $("#javaAndroidTrack").hide();
+      $("#phpDrupalTrack").hide();
+    } else if (totalScore >=6 && totalScore <=25){
+      $("#javaAndroidTrack").show();
+      $("#phpDrupalTrack").hide();
+      $("#cssDesignTrack").hide();
+    } else if (totalScore >=26) {
+      $("#phpDrupalTrack").fadeIn();
+      $("#javaAndroidTrack").hide();
+      $("#cssDesignTrack").hide();
+    } else if (totalScore == ""){
+      $("#pleaseSelect").show();
+    }
+    console.log(totalScore)
+    $(".firstName").text(firstName);
+    $(".city").text(city);
+
+
+
   });
-
-
 });
 
-
-
-//
-// $(".firstName").text(firstName);
-// $(".city").text(city);
-//
-// if (codingTrack === "1", "2","3") {
-//   $("#pleaseSelect").show();
-//   $("#cssDesignTrack").hide();
-//   $("#phpDrupalTrack").hide();
-//   $("#javaAndroidTrack").hide();
-// }
-//
-// if (codingTrack === "1"){
-//   $("#javaAndroidTrack").show();
-//   $("#cssDesignTrack").hide();
-//   $("#phpDrupalTrack").hide();
-//   $("#pleaseSelect").hide();
-// } else if (codingTrack === "2") {
-//   $("#cssDesignTrack").show();
-//   $("#javaAndroidTrack").hide();
-//   $("#phpDrupalTrack").hide();
-//   $("#pleaseSelect").hide();
-// } else if (codingTrack === "3") {
-//   $("#phpDrupalTrack").show();
-//   $("#cssDesignTrack").hide();
-//   $("#javaAndroidTrack").hide();
-//   $("#pleaseSelect").hide();
-// }
-//
-//
-//   $(".btn").click(function() {
-//     $("#phpDrupalTrack").fadeIn();
-//     $("#cssDesignTrack").fadeIn();
-//     $("#javaAndroidTrack").fadeIn();
-//     $("#pleaseSelect").fadeIn();
-//   });
+  $(".btn").click(function() {
+    $("#phpDrupalTrack").fadeIn();
+    $("#cssDesignTrack").fadeIn();
+    $("#javaAndroidTrack").fadeIn();
+    $("#pleaseSelect").fadeIn();
+  });
